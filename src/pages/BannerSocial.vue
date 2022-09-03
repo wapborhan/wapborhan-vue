5896338e062d4ec952e596c1462e30e6427fbb5f
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-03 21:03:14 +0600

    Social Link Add

--- src/pages/BannerSocial.vue
+++ src/pages/BannerSocial.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-
+  <div className="social-share-style-1">
+    <ul className="social-share d-flex liststyle">
+      <li>
+        <a href="http://facebook.com/wapborhan" target="_blank" rel="noopener noreferrer">
+          <FaFacebookSquare />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.github.com/wapborhan" target="_blank" rel="noopener noreferrer">
+          <FaGithubSquare />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/wapborhan/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.twitter.com/wapborhan/" target="_blank" rel="noopener noreferrer">
+          <FaTwitterSquare />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.codepen.io/wapborhan/" target="_blank" rel="noopener noreferrer">
+          <FaCodepen />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/wapborhan/wapborhan-apps/raw/main/app/wapborhan-V1.1.apk" target="_blank"
+          rel="noopener noreferrer">
+          <AiFillAndroid />
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'BannerSocial'
 
 }
 </script>
